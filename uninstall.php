b7a9bc8137490c832f3d94842fbf77399664d8ad
Vilmos Ioo <cool.villi@gmail.com>
2013-12-30 02:04:18 +0000

    Added custom taxonomy.

--- uninstall.php
+++ uninstall.php
@@ -4,7 +4,7 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 		exit();
 // For Single site
 if (!is_multisite()){
-	delete_option('WP_PORTFOLIO');
+	delete_option('VI_PORTFOLIO');
 } 
 // For Multisite
 else {
@@ -13,7 +13,7 @@ else {
 	$original_blog_id = get_current_blog_id();
 	foreach ($blog_ids as $blog_id){
 		switch_to_blog( $blog_id );
-		delete_option('WP_PORTFOLIO');
+		delete_option('VI_PORTFOLIO');
 	}
 	switch_to_blog( $original_blog_id );
 }
